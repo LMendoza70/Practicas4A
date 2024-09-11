d452745777038df9696f4cdbe46554434140269c
LMendoza70 <luis.mendoza@uthh.edu.mx>
2024-09-11 10:10:06 -0600

    Gua de instalacion e instalable del gcc

--- TiposDatos/TiposDatos.c
+++ TiposDatos/TiposDatos.c
@@ -1,26 +1,30 @@
 //en este espacio se encuentran las librerias de nuestra aplicacion 
 #include<stdio.h>
-/*este es un comentario 
-que posee 
-varias lineas*/
 #include<conio.h>
+#include<math.h>
 
 int main(){
     float frente=0;
     float fondo=0;
     float area=0;
-
+    float Preciom2=0,total=0; //podemos declarar variables en una sola linea de codigo
+    //siempre y cuando sean del mismo tipo de dato
     printf("Captura el valor de frente : \n");
     scanf("%f", &frente);
 
     printf("Captura el valor del fondo : \n");
     scanf("%f", &fondo);
 
+    printf("Cual es el precio del metro cuadrado de terreno?\n");
+    scanf("%f",&Preciom2);
+    
     area=frente*fondo;
+    total=area*Preciom2;
 
-    printf("Su terreno tiene %f metros cuadrados",area);
+
+    printf("Su terreno tiene %f metros cuadrados \n",area);
+    printf("por lo que tiene un costo de %f pesitos",total);
     getch();
     
     return 0;
-
 }
